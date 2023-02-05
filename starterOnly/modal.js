2f2f44d7c7bfe7ece8097f446da1c18ee7886a32
Tetyana <tetyana.chouteau@gmail.com>
2023-02-05 15:55:12 +0100

    issue 2 validation formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,7 +20,7 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 closeBtn.addEventListener("click", closeModal);
 
 // launch modal form
-functiodata-error-visiblen launchModal() {
+function launchModal() {
   modalbg.style.display = "block";
 }
 
@@ -29,14 +29,61 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-function NomPrenomInput(input){
-  if (input.value.toString().trim().length < 2){
-    input.parentElement.setAttribute("data-error-visible", "true");
-      return false;
+// validate
+function validate() {
+  // au début pas d'erreur
+  let isValidate = true;
+
+  // on teste nom 
+  const inputNom = document.querySelector("#last");
+  // calcul s'il y a au moins un erreur ou pas 
+  isValidate =  NomPrenomInput(inputNom) && isValidate;
+
+  // on test prénom
+  const inputPrenom = document.querySelector("#first");
+  isValidate =  NomPrenomInput(inputPrenom) && isValidate;
+
+  // on test email
+  const inputEmail = document.querySelector("#email");
+  isValidate =  Html5Input(inputEmail) && isValidate;
+
+  // on test Quantity
+  const inputQuantity = document.querySelector("#quantity");
+  isValidate =  Html5Input(inputQuantity) && isValidate;
+
+  // on test location
+  const inputRadio = document.querySelector("[name='location']");
+  isValidate =  Html5Input(inputRadio) && isValidate;
+
+  // on test conditions generales
+  const inputCheckbox1 = document.querySelector("#checkbox1");
+  isValidate =  Html5Input(inputCheckbox1) && isValidate;
+  
+  return isValidate;
+}
+
+function NomPrenomInput(input) {
+  if (input.value.toString().trim().length < 2) {
+    input.parentElement.dataset.errorVisible = true;
+    //input.parentElement.setAttribute("data-error-visible", "true");
+    return false;
   }
-    else{
-      input.parentElement.remouveAttribute("data-error-visible")
-        return true;
+  else {
+    delete input.parentElement.dataset.errorVisible;
+    //input.parentElement.remouveAttribute("data-error-visible")
+    return true;
+
+  }
+}
+
+function Html5Input(input) {
+  if (!input.checkValidity()) {
+    input.parentElement.dataset.errorVisible = true;
+    return false;
+  }
+  else {
+    delete input.parentElement.dataset.errorVisible;
+    return true;
 
   }
 }
